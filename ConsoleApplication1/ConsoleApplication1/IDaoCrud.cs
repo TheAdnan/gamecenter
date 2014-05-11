77e780fa6b852d05350390cb43f43d0a39df2782
TheAdnan <adnankicin92@gmail.com>
2014-05-11 14:40:52 +0200

    Adnan Kičin

---
+++ ConsoleApplication1/ConsoleApplication1/IDaoCrud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface IDaoCrud<T>
+    {
+        long create(T entity);  // INSERT INTO
+        T read(T entity);        // SELECT FROM WHERE ..
+        T update(T entity);     // UPDATE
+        void delete(T entity);  // DELETE
+        T getById(int id);       // SELECT .. WHRE ID=
+        List<T> getAll();        // SELECT *
+        List<T> getByExample(string name, string value);  // SELECT .. WHERE name=value
+    }
+
+}
