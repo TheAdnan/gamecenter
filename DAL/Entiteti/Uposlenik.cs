81e3276b4ae06aa2bede4f584d8b5ac063aaf32c
tortii <nklisura1@etf.unsa.ba>
2014-05-14 22:25:10 +0200

    commit

---
+++ DAL/Entiteti/Uposlenik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entiteti
+{
+    public class Uposlenik : Radnik
+    {
+        public void ProdajIgricu(Igrica i)
+        {
+        }
+        public void RegistrujKlijenta(Klijent k)
+        {
+        }
+    }
+}
