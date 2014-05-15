5937bbfb2bab44aa16d31d8993c54f767da02e62
tortii <nklisura1@etf.unsa.ba>
2014-05-15 10:57:33 +0200

    dal - dao

---
+++ Game Center/DAL/Entiteti/Poslodavac.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Poslodavac : Radnik
+    {
+        public void ZaposliRadnika()
+        {
+        }
+        public void OtpustiRadnika()
+        {
+        }
+    }
+}
---
+++ Game Center/DAL/Entiteti/Uposlenik.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+
+    public class Uposlenik : Radnik
+    {
+        public void ProdajIgricu(Igrica i)
+        {
+        }
+        public void RegistrujKlijenta(Klijent k)
+        {
+        }
+    }
+}
