f86fbd0518b7684223d541a24a1910761f3fd322
tortii <nklisura1@etf.unsa.ba>
2014-05-15 11:01:59 +0200

    dal

---
+++ Game Center/DAL/Entiteti/Radnik.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+
+    private class Radnik : Osoba
+    {
+        private string Job;
+        private real Plata;
+        private integer RadnoVrijeme = 8;
+        public Radnik()
+        {
+        }
+        ~Radnik()
+        {
+        }
+        public string DajID()
+        {
+        }
+    }
+}
---
+++ Game Center/Gamecenter-Dal-Dao/club-of-duty/club-of-duty/StudentDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    public class StudentDAO : IDaoCrud<Student>
+    {
+        private MySqlConnection con;
+
+        public StudentDAO(string host, string db, string user, string pass)
+        {
+            string connectionString = "server=localhost;user=" + user + ";pwd=" + pass + ";database=" + db;
+            con = new MySqlConnection(connectionString);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception e) { throw e; }
+        }
+        /*long create(Student entity)
+        {
+            string user = "root";
+            string pass = "";
+            string db = "dao";
+            string connectionString = "server=localhost;user=" + user + ";pwd=" + pass + ";database=" + db;
+            MySqlConnection con = new MySqlConnection(connectionString);
+            // Unos novog studenata
+            con.Open();
+            string ime = "Adnan", prezime = "Kicin";
+            MySqlCommand c = new MySqlCommand("insert into student values ('" + ime + "','" + prezime + "')", con);
+            c.ExecuteNonQuery();
+            // insert into...
+        }
+         */
+        //    CRUD metode implementirane
+    }
+}
